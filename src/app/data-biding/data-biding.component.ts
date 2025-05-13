7c2a510f638514b9ad624f2485e76342422ab2a1
Jucalino <joseluis.bmjnet@gmail.com>
2025-05-13 09:56:10 -0300

    Initial commit

---
+++ src/app/data-biding/data-biding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-biding',
+  templateUrl: './data-biding.component.html',
+  styleUrls: ['./data-biding.component.scss']
+})
+export class DataBidingComponent implements OnInit {
+
+  url: string = 'http://loiane.com'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
